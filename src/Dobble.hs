75733b26d7fa36e7f71975d88b13721d7f1027e4
erykciepiela <eryk.ciepiela.dev@gmail.com>
2020-04-05 08:59:20 +0200

    cleanup

--- src/Dobble.hs
+++ src/Dobble.hs
@@ -10,21 +10,15 @@ module Dobble (
 
 import Data.Numbers.Primes
 import Data.List
-
+import Data.Set
 
 newtype Card s = Card {
     cardSymbols :: [s]
 }
 
-deriving instance (Eq s) => Eq (Card s)
-deriving instance (Ord s) => Ord (Card s)
-deriving instance (Show s) => Show (Card s)
-
 newtype Deck s = Deck {
     cards :: [Card s]
 }
-
-deriving instance (Show s) => Show (Deck s)
 
 -- | Dobble symbol number is each number equal to @p*p+p+1@ for sime prime @p@, examples 3, 7, 13, 31, 57, ...
 isDobbleSymbolNumber :: Int -> Bool
@@ -37,7 +31,7 @@ isDobbleSymbolNumber n = head (dropWhile (< n) $ fmap (\p -> p * p + p + 1) (1:p
 -- | for 31 symbols returns deck of 31 cards with 6 symbols on a card
 -- | for 57 symbols returns deck of 57 cards with 8 symbols on a card
 deck :: [s] -> Deck s
-deck symbols = let (Just prime) = find (\p -> p * p + p + 1 >= length symbols) (1:primes) in Deck $ Card . fmap (symbols !!) <$> projectivePlane prime
+deck symbols = let (Just prime) = find (\p -> p * p + p + 1 >= length symbols) (1:primes) in Deck $ Card . fmap (symbols !!) . toList <$> toList (projectivePlane prime)
 
 -- | For prime number @p@ returns @p2+p+1@ lines of @p2+p+1@ points, with @p+1@ points on each line
 -- | E.g. 
@@ -46,7 +40,7 @@ deck symbols = let (Just prime) = find (\p -> p * p + p + 1 >= length symbols) (
 -- | for p = 3 returns 13 lines with 13 points, with 4 points of each line
 -- | for p = 5 returns 31 lines with 31 points, with 6 points of each line
 -- | for p = 7 returns 57 lines with 57 points, with 8 points of each line
-projectivePlane :: Int -> [[Int]]
-projectivePlane p = [[((i * k + j) `mod` p) * p + k | k <- [0..(p - 1)]] <> [p * p + i] | i <- [0..(p - 1)], j <- [0..(p - 1)]]
-    <> [[j * p + i | j <- [0..(p - 1)]] <> [p * p + p] | i <- [0..(p - 1)]] 
-    <> [[p * p + i | i <- [0..p]]]
+projectivePlane :: Int -> Set (Set Int)
+projectivePlane p = fromList $ [fromList ([((i * k + j) `mod` p) * p + k | k <- [0..(p - 1)]] <> [p * p + i]) | i <- [0..(p - 1)], j <- [0..(p - 1)]]
+    <> [fromList ([j * p + i | j <- [0..(p - 1)]] <> [p * p + p]) | i <- [0..(p - 1)]] 
+    <> [fromList [p * p + i | i <- [0..p]]]
