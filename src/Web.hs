44b498a0367378dbbb433d63f0cfc2e7bb18fc62
erykciepiela <eryk.ciepiela.dev@gmail.com>
2020-05-03 14:48:55 +0200

    web app

--- src/Web.hs
+++ src/Web.hs
@@ -10,7 +10,6 @@ import Servant.Multipart
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString as BS
 import Control.Monad.IO.Class
-import Control.Monad
 import Data.Foldable
 import SVG (generateSVGDeck')
 import Data.String
@@ -19,54 +18,68 @@ import Data.Time
 import Control.Concurrent
 import Control.Monad
 import System.Directory
+import Data.Text (unpack)
+import Data.List
+import Data.Ord
+import System.Random
+import Data.UUID (UUID)
 
 
 type PurgeAPI = Get '[HTML] (Html ())
     :<|> "form" :> MultipartForm Mem (MultipartData Mem) :> Post '[HTML] (Html ())
     :<|> "decks" :> Capture "deckId" String :> Get '[OctetStream] LBS.ByteString
 
-decksDirectory = "decks"
-flipFile = "flip/flip.svg"
 
 runWebApp :: IO ()
 runWebApp = run 8081 $ serveWithContext (Proxy :: Proxy PurgeAPI) ((defaultMultipartOptions (Proxy :: Proxy Mem)) {generalOptions = setMaxRequestNumFiles 32 defaultParseRequestBodyOptions} :. EmptyContext) $ mainHTML
     :<|> generateDeck 
     :<|> deck
     where
+        decksDirectory :: FilePath
+        decksDirectory = "decks"
+
+        flipFile :: FilePath
+        flipFile = "flip/flip.svg"
+
         mainHTML :: MonadIO m => m (Html ())
         mainHTML = liftIO $ do
-            deckDirectories <- listDirectory decksDirectory
+            deckFiles <- listDirectory decksDirectory
+            modTimes <- traverse getModificationTime $ ((decksDirectory <> "/") <>) <$> deckFiles
+            timeZone <- getCurrentTimeZone
+            let deckFileEntries = sortOn (Down . snd) (zip deckFiles modTimes)
             return $ html_ $ do
                 title_ "Dobble Generator"
                 body_ $ do
                     h1_ "Dobble Generator"
                     h2_ "New deck"
                     form_ [action_ "form", method_ "post", enctype_ "multipart/form-data" ] $ do
-                        span_ "Pick 31 symbols svg files "
+                        span_ "Pick 31 symbols as svg files "
                         input_ [type_ "file", id_ "symbolSvgFiles", name_ "symbolSvgFiles", multiple_ "true", accept_ ".svg"]
-                        span_ " then a name of the deck "
+                        span_ " and a name for the deck "
                         input_ [type_ "input", id_ "deckName", name_ "deckName"]
-                        span_ " and press "
-                        input_ [type_ "submit", value_ "Create Deck!"]
+                        input_ [type_ "submit", value_ "Generate"]
                     h2_ "Generated decks"
-                    p_ $ a_ [href_ "/"] "refresh"
-                    forM_ deckDirectories $ \deckDirectory -> p_ $ a_ [href_ ("/decks/" <> fromString deckDirectory)] (fromString deckDirectory)
+                    p_ $ a_ [href_ "/"] "refresh list"
+                    forM_ deckFileEntries $ \(deckFile, utcModTime) -> p_ $ do
+                        a_ [href_ ("/decks/" <> fromString deckFile)] (fromString (drop 37 deckFile))
+                        span_ (fromString (" generated on " <> show (utcToLocalTime timeZone utcModTime)))
 
         deck :: MonadIO m => String -> m LBS.ByteString
         deck deckId = liftIO $ LBS.readFile (decksDirectory <> "/" <> deckId)
 
         generateDeck :: MonadIO m => MultipartData Mem -> m (Html ())
         generateDeck multipartData = liftIO $ do
-            time <- getCurrentTime 
-            let directoryName = decksDirectory <> "/" <> show time
+            let (Just deckName) = unpack . iValue <$> find ((== "deckName") . iName) (inputs multipartData)
             forkIO $ do
+                (uuid :: UUID) <- randomIO 
                 let symbolSvgBSs = LBS.toStrict . fdPayload <$> filter ((== "symbolSvgFiles") . fdInputName) (files multipartData)
                 flipSideSvgBS <- BS.readFile "flip/flip.svg"
                 pages <- generateSVGDeck' symbolSvgBSs flipSideSvgBS
-                LBS.writeFile (directoryName <> ".zip") $ fromArchive $ foldr (\(i, page) a -> addEntryToArchive (toEntry (show i <> ".png") 0 (LBS.fromStrict page)) a) emptyArchive (zip [1..] pages)
+                let deckFile = decksDirectory <> "/" <> show uuid <> "_" <> deckName <> ".zip"
+                LBS.writeFile deckFile $ fromArchive $ foldr (\(i, page) a -> addEntryToArchive (toEntry (show i <> ".png") 0 (LBS.fromStrict page)) a) emptyArchive (zip [1..] pages)
             return $ html_ $ do
                 title_ "Dobble Generator"
                 body_ $ do
-                    h1_ "Deck is being generated..."
-                    p_ (fromString directoryName)
+                    h1_ $ "Generating deck"
+                    p_ $ fromString $ "Deck '" <> deckName <> "' is being generated."
                     a_ [href_ "/"] "return to the main page"
